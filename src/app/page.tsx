838675d7bc861527f9f0d867f0b572eee3cab205
ChirujanoCodding <trdfabris@gmail.com>
2023-10-10 15:04:35 -0300

    feat: added `biome` as formatter & formatted files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div > Hello, world </div>
-    </main>
-  )
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<div> Hello world </div>
+		</main>
+	);
 }

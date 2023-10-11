583c78fb8d1afd8fe8abd444e84cdbfa60e825a3
romancitodev <trdfabris@gmail.com>
2023-10-11 17:17:57 -0300

    feat(front): :construction: login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,8 @@
 export default function Login() {
-	return <h1>Este es el login</h1>;
+	return <div className='w-50 h-50 content-center hover:content-around' >
+	<form className=" grid gap-5 w-max h-max"  >
+		<input className="border-2 rounded border-l-blue-300" type="text" placeholder="Username" />
+		<input type="password" placeholder="Password" />
+	</form>
+	</div>
 }

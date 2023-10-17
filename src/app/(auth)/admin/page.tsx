86e94cfb89305c1dde656610691aa166f85408b0
ChirujanoCodding <trdfabris@gmail.com>
2023-10-17 06:12:56 -0300

    feat(front): :sparkles: separated `login admin` from `login user`

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -5,21 +5,11 @@ export default function Login() {
 	return (
 		<main className='h-screen overflow-hidden'>
 			<div className='grid content-center justify-center h-screen gap-5'>
-				<div
-					className='flex h-[550px] shadow-2xl rounded-2xl
-				 items-center justify-around'
-				>
-					<div className={'w-[400px] h-max'}>
-						<img
-							src='/hospital_ambulancia.jpg'
-							alt='login-hospital'
-							className='h-[550px] object-cover rounded-l-2xl'
-						/>
-					</div>
+				<div className='flex h-[550px] shadow-2xl rounded-2xl items-center justify-around'>
 					<div className='grid justify-around p-10 gap-10'>
 						<div className='grid text-center justify-center gap-y-2'>
 							<img src='/Isotipo.png' alt='isotipo' />
-							<p>Personal médico</p>
+							<p>Administracion</p>
 						</div>
 						<form className='grid gap-5 w-[500px] h-max py-10 items-center'>
 							<input
@@ -39,10 +29,17 @@ export default function Login() {
 							</button>
 						</div>
 					</div>
+					<div className={'w-[400px] h-max'}>
+						<img
+							src='/hospital.jpg'
+							alt='login-hospital'
+							className='h-[550px] object-cover rounded-r-2xl'
+						/>
+					</div>
 				</div>
 				<p className='flex place-content-center text-center gap-x-2'>
-					¿Formás parte de la administración?
-					<Link href='/admin' className='font-bold'>
+					¿Formás parte solo del personal médico?
+					<Link href='/login' className='font-bold'>
 						Inicie aqui.
 					</Link>
 				</p>

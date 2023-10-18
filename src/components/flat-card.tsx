76d0cea9115b80aa030909ee187a066aaec4cb5b
romancitodev <trdfabris@gmail.com>
2023-10-18 11:50:13 -0300

    style(front): :construction: fixing `flat-card` issue

--- src/components/flat-card.tsx
+++ src/components/flat-card.tsx
@@ -6,29 +6,31 @@ type Props = { name: string; dni: string };
 
 export function Card({ name, dni }: Props) {
 	return (
-		<div className='flex justify-around w-full shadow-lg rounded-2xl border-2 border-zinc-300 py-10 m-auto text-[18px] font-medium'>
-			<div className='flex justify-start gap-5 h-max text-center items-center'>
-				<SmallIcon>
-					<Account />
-				</SmallIcon>
-				<p>{name}</p>
-			</div>
-			<div className='flex justify-start gap-5 h-max text-center items-center'>
-				<SmallIcon>
-					<Info />
-				</SmallIcon>
-				<p>{dni}</p>
+		<div className='flex justify-between w-full shadow-lg rounded-2xl border-2 border-zinc-300 py-5 px-10 m-auto text-[18px] font-medium'>
+			<div className='flex justify-between gap-x-32'>
+				<div className='flex gap-5 h-max max-w-2xl  w-max text-center items-center'>
+					<SmallIcon>
+						<Account />
+					</SmallIcon>
+					<p className='w-max max-w-[200px]'>{name}</p>
+				</div>
+				<div className='flex justify-start gap-5 h-max text-center items-center'>
+					<SmallIcon>
+						<Info />
+					</SmallIcon>
+					<p className='w-max'>{dni}</p>
+				</div>
 			</div>
 			<div className='flex justify-between w-max gap-10'>
 				<button type='button'>
-				<SmallIcon>
-					<Account />
-				</SmallIcon>
+					<SmallIcon>
+						<Account />
+					</SmallIcon>
 				</button>
 				<button type='button'>
-				<SmallIcon>
-					<Account />
-				</SmallIcon>
+					<SmallIcon>
+						<Account />
+					</SmallIcon>
 				</button>
 			</div>
 		</div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -14,7 +14,7 @@ export function SideBar() {
 	const pathname = usePathname();
 
 	return (
-		<div className='w-[120px] h-screen flex-col justify-around inline-flex bg-white shadow-lg shadow-zinc-400'>
+		<div className='w-[120px] h-screen flex-col justify-around inline-flex bg-white shadow-lg shadow-zinc-400 rounded-r-2xl'>
 			<div className='grid justify-center w-full gap-5 p-5'>
 				<Logo />
 			</div>

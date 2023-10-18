07a7fa7b1b173388ec6a2afa35689a7e97302381
romancitodev <trdfabris@gmail.com>
2023-10-18 09:42:15 -0300

    refactor(front): :lipstick: moved components

--- src/app/(dashboard)/patients/page.tsx
+++ src/app/(dashboard)/patients/page.tsx
@@ -6,16 +6,17 @@ export default function Patients() {
 	return (
 		<div className='grid gap-10'>
 			<Title text='Pacientes' />
-			<div className='grid w-full'>
-				<div className='flex gap-x-5 justify-start'>
+			<div className='grid w-full h-full gap-y-10 m-auto px-20'>
+				<div className='flex justify-between w-full '>
+					<div className='flex h-full w-[200px]'>
+						<Add />
+					</div>
 					<div className='flex h-16'>
 						<Filter placeholder='Filter by name' left full />
 						<Filter placeholder='Filter by DNI' right />
 					</div>
-					<div className='flex h-full w-[200px]'>
-						<Add />
-					</div>
 				</div>
+				<div className=' w-full h-full bg-yellow-500'>a</div>
 			</div>
 		</div>
 	);

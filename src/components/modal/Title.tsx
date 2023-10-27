d893952587c8ff073851cf1f4fab22466f9aa3c1
ChirujanoCodding <trdfabris@gmail.com>
2023-10-27 02:11:14 -0300

    style: 💄 added tiny border to the `Title` in Modal

--- src/components/modal/Title.tsx
+++ src/components/modal/Title.tsx
@@ -1,3 +1,3 @@
 export function Title({ text }: { text: string }) {
-	return <h1 className='text-[30px] w-full h-min font-extrabold'>{text}</h1>;
+	return <h1 className='text-[30px] w-full h-min font-extrabold border-b-2 border-zinc-900 pb-2'>{text}</h1>;
 }

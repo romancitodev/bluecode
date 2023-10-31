164b2be5356979bdb66028c40c3e4d1afd657384
tamagoru <108638495+ztamagoru@users.noreply.github.com>
2023-10-31 12:38:24 -0300

    feat(front): :lipstick: fixed typos and added new categories on patient modal

--- src/components/personModal.tsx
+++ src/components/personModal.tsx
@@ -11,7 +11,7 @@ export function PatientModal({ show, onClose }: Props) {
 			<Modal.Container>
 				<Modal.Grid className='grid overflow-y-auto h-full w-full gap-5 scroll-smooth no-scrollbar'>
 					<Modal.Grid>
-						<Modal.Title text='Info Basica' />
+						<Modal.Title text='Info Básica' />
 						<Modal.Group className='w-full h-full grid grid-cols-2 justify-between gap-5'>
 							<Modal.TextInput placeholder='Nombre' />
 							<Modal.TextInput placeholder='Apellido' />
@@ -19,9 +19,18 @@ export function PatientModal({ show, onClose }: Props) {
 							<Modal.TextInput placeholder='Sexo' />
 							<Modal.TextInput placeholder='Domicilio' full />
 							<Modal.TextInput placeholder='Estado Civil' />
-							<Modal.TextInput placeholder='Grupo Sanginueo' />
+							<Modal.TextInput placeholder='Grupo Sanguineo' />
 						</Modal.Group>
 					</Modal.Grid>
+
+					<Modal.Grid>
+						<Modal.Title text='Afiliación'/>
+						<Modal.Group className='w-full h-full grid grid-cols-2 justify-between gap-5'>
+							<Modal.TextInput placeholder='Tipo' />
+							<Modal.TextInput placeholder='Nombre' />
+						</Modal.Group>
+					</Modal.Grid>
+
 					<Modal.Grid>
 						<Modal.Title text='Alergias' />
 						<Modal.Group>
@@ -31,6 +40,7 @@ export function PatientModal({ show, onClose }: Props) {
 							<Modal.TextInput placeholder='Alergia X' />
 						</Modal.Group>
 					</Modal.Grid>
+					
 					<Modal.Grid>
 						<Modal.Title text='Enfermedades' />
 						<Modal.Group>
@@ -40,6 +50,7 @@ export function PatientModal({ show, onClose }: Props) {
 							<Modal.TextInput placeholder='Enfermedad X' />
 						</Modal.Group>
 					</Modal.Grid>
+
 					<Modal.Grid>
 						<Modal.Title text='Vacunas' />
 						<Modal.Group>
@@ -49,7 +60,29 @@ export function PatientModal({ show, onClose }: Props) {
 							<Modal.TextInput placeholder='Vacunas X' />
 						</Modal.Group>
 					</Modal.Grid>
+
+					<Modal.Grid>
+						<Modal.Title text='Accidentes' />
+						<Modal.Group className='w-full h-full grid grid-cols-2 justify-between'>
+							<Modal.LargeTextField placeholder='Accidentes ocurridos' />
+						</Modal.Group>
+					</Modal.Grid>
+
+					<Modal.Grid>
+						<Modal.Title text='Antecedentes' />
+						<Modal.Group className='w-full h-full grid grid-cols-2 justify-between'>
+							<Modal.LargeTextField placeholder='Antecedentes' />
+						</Modal.Group>
+					</Modal.Grid>
+
+					<Modal.Grid>
+						<Modal.Title text='Observaciones' />
+						<Modal.Group className='w-full h-full grid grid-cols-2 justify-between'>
+							<Modal.LargeTextField placeholder='Observaciones' />
+						</Modal.Group>
+					</Modal.Grid>
 				</Modal.Grid>
+
 				<Modal.Group font='bold'>
 					<Modal.Button
 						text='Close'

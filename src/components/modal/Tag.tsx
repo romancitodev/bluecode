24fc7c703d070f2b75b1adc58bd3ae7a43f366c1
ChirujanoCodding <trdfabris@gmail.com>
2023-10-31 22:19:02 -0300

    fix(front): :lipstick: add padding to the value of the tag

--- src/components/modal/Tag.tsx
+++ src/components/modal/Tag.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export function Tag({ value, onClick }: Props) {
 	return (
-		<div className='w-[150px] px-5 h-full flex content-center items-center justify-between border-2 border-zinc-400 bg-zinc-100 rounded-xl hover:border-zinc-700 hover:bg-zinc-200/75 transition-all duration-250'>
-			{value}
+		<div className='w-max px-5 h-full flex content-center items-center justify-between border-2 border-zinc-400 bg-zinc-100 rounded-xl hover:border-zinc-700 hover:bg-zinc-200/75 transition-all duration-250'>
+			<p className='pr-8'>{value}</p>
 			<button onClick={onClick} type='button'>
 				<X className='hover:text-red-500 transition-colors duration-250' />
 			</button>

51c202bac3ee31b94e1ce420eb793fd63c7078e3
tamagoru <108638495+ztamagoru@users.noreply.github.com>
2023-11-02 14:36:02 -0300

    feat(front): :fire: removed unused import

--- src/components/modal/DatePicker.tsx
+++ src/components/modal/DatePicker.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import { format } from 'date-fns';
-import { Calendar as CalendarIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';

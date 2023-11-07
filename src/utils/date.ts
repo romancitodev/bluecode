e45ac69c28e2dcd055bf6c441c1645f6d70ca38d
romancitodev <trdfabris@gmail.com>
2023-11-07 11:37:01 -0300

    refactor(back): refactored substractYears function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
 export const substractYears = (years: number, date = new Date()): Date => {
-    const res = new Date()
-    res.setFullYear(date.getFullYear() - years);
-    return res
-    }
+	const res = new Date();
+	res.setFullYear(date.getFullYear() - years);
+	return res;
+};

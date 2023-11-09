9cece570d75a7c7fbf1ccb5f89017456cdc17610
ztamagoru <zdamaria119@gmail.com>
2023-11-09 19:31:43 -0300

    fix(front): :lipstick: fixed modal height responsiveness... again.

--- src/components/modal/Container.tsx
+++ src/components/modal/Container.tsx
@@ -6,11 +6,11 @@ type Props = {
 export function Container({ children }: Props) {
 	return (
 		<div className='bg-white max-w-[1000px]
-		sm:max-w-[300px] sm:max-h-[500px]
-		md:max-w-[500px] md:max-h-[600px]
-		lg:max-w-[800px] lg:max-h-[800px]
+		sm:max-w-[300px] sm:max-h-[450px]
+		md:max-w-[500px] md:max-h-[500px]
+		lg:max-w-[800px] lg:max-h-[550px]
 		xl:max-w-[900px] xl:max-h-[600px]
-		2xl:max-w-[1000px] 2xl:max-h-[800px]
+		2xl:max-w-[1000px] 2xl:max-h-[700px]
 
 		w-full shadow-lg rounded-2xl grid gap-5 p-10 justify-between'>
 			{children}

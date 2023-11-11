66ca302e8d8d0e6b51f42ed45d531c500cf6774c
tamagoru <108638495+ztamagoru@users.noreply.github.com>
2023-11-11 02:12:32 -0300

    feat(front): :lipstick: finished custom error messages

--- src/app/schemas/patients-form.ts
+++ src/app/schemas/patients-form.ts
@@ -6,36 +6,65 @@ const BLOOD_GROUPS = ['a+', 'a-', 'b+', 'b-', 'ab+', 'ab-', 'o+', 'o-'];
 
 export const PatientForm = z
 	.object({
-		name: z.string().min(1),
-		surname: z.string().min(1),
-		birth: z.date(),
-		dni: z.coerce.number().min(1_000_000).max(999_999_999),
-		phone: z.coerce.number().min(1100000001).max(1199999999),
-		gender: z.string().refine(
+		name: z.string().min(1, {
+			message: 'Nombre inválido'
+		}),
+		surname: z.string().min(1, {
+			message: 'Apellido inválido'
+		}),
+		birth: z.date({
+			required_error: 'Fecha de nacimiento inválida',
+		}),
+		dni: z.coerce.number().min(1_000_000, {
+			message: 'DNI inválido'
+		}).max(999_999_999, {
+			message: 'DNI inválido'
+		}),
+		phone: z.coerce.number().min(1100000001, {
+			message: 'Número de teléfono inválido'
+		}).max(1199999999, {
+			message: 'Número de teléfono inválido'
+		}),
+		gender: z.string({
+			required_error: 'Sexo inválido'
+		}).refine(
 			args => {
 				return GENDERS.includes(args.toLowerCase());
-			},
-			{ message: 'Invalid gender' },
+			}
 		),
-		civil_status: z.string().refine(args => STATUS.includes(args.toLowerCase()), {
-			message: 'Invalid civil status',
+		civil_status: z.string({
+			required_error: 'Estado civil inválido'
+		}).refine(args => STATUS.includes(args.toLowerCase())),
+		street: z.string().min(1, {
+			message: 'Domicilio inválido'
 		}),
-		street: z.string().min(1),
-		street_number: z.coerce.number().min(1),
-		neighborhood: z.string().min(1),
-		province: z.string().min(1),
-		house_type: z.string().min(1),
+		street_number: z.coerce.number().min(1, {
+			message: 'Número de domicilio inválido'
+		}).max(9999, {
+			message: 'Número de domicilio inválido'
+		}),
+		neighborhood: z.string().min(1, {
+			message: 'Localidad inválida'
+		}),
+		province: z.string().min(1, {
+			message: 'Provincia inválida'
+		}),
+		house_type: z.string({
+			required_error: 'Tipo de vivienda inválida'
+		}).min(1),
 		department_annotation: z.string().default('-'),
 		department_number: z.coerce.number().nullable().default(null),
 		area: z.string().min(1),
 		bed_number: z.coerce.number().default(0),
-		blood_type: z
-			.string()
-			.refine(args => BLOOD_GROUPS.includes(args.toLowerCase()), {
-				message: 'Invalid blood type',
-			}),
-		affiliation_type: z.string().min(1),
-		affiliation_name: z.string().min(1),
+		blood_type: z.string({
+			required_error: 'Grupo sanguíneo inválido'
+		}).refine(args => BLOOD_GROUPS.includes(args.toLowerCase())),
+		affiliation_type: z.string({
+			required_error: 'Tipo de afiliación inválida'
+		}).min(1),
+		affiliation_name: z.string().min(1, {
+			message: 'Afiliación inválida'
+		}),
 		allergies: z.array(z.string()).optional(),
 		illnesses: z.array(z.string()).optional(),
 		vacuums: z.array(z.string()).optional(),
--- src/app/schemas/personal-form.ts
+++ src/app/schemas/personal-form.ts
@@ -4,47 +4,47 @@ const GENDERS = ['masculino', 'femenino', 'otro'];
 const STATUS = ['casado', 'soltero'];
 
 export const PersonalForm = z
-    .object({
-        name: z.string().min(1, {
+	.object({
+		name: z.string().min(1, {
 			message: 'Nombre inválido'
 		}),
-        surname: z.string().min(1, {
+		surname: z.string().min(1, {
 			message: 'Apellido inválido'
 		}),
-        birth: z.date({
+		birth: z.date({
 			required_error: 'Fecha de nacimiento inválida'
 		}),
-        dni: z.coerce.number().min(1_000_000, {
+		dni: z.coerce.number().min(1_000_000, {
 			message: 'DNI inválido'
 		}).max(999_999_999, {
 			message: 'DNI inválido'
 		}),
-        cuit: z.coerce.number().min(20000000000, {
+		cuit: z.coerce.number().min(20000000000, {
 			message: 'CUIT inválido'
 		}).max(34999999999, {
 			message: 'CUIT inválido'
 		}),
-        phone: z.coerce.number().min(1100000001, {
+		phone: z.coerce.number().min(1100000001, {
 			message: 'Número de teléfono inválido'
 		}).max(1199999999, {
 			message: 'Número de teléfono inválido'
 		}),
-        date_start: z.date({
+		date_start: z.date({
 			required_error: 'Fecha de inicio inválida'
 		}),
-        gender: z.string({
-			required_error: 'Género inválido'
+		gender: z.string({
+			required_error: 'Sexo inválido'
 		}).refine(
 			args => {
 				return GENDERS.includes(args.toLowerCase());
 			},
 		),
-        civil_status: z.string({
+		civil_status: z.string({
 			required_error: 'Estado civil inválido'
 		}).refine(args => STATUS.includes(args.toLowerCase()), {
 			message: 'Estado civil inválido',
 		}),
-        street: z.string().min(1, {
+		street: z.string().min(1, {
 			message: 'Domicilio inválido'
 		}),
 		street_number: z.coerce.number().min(1, {
@@ -66,9 +66,9 @@ export const PersonalForm = z
 		area: z.array(z.string(), {
 			required_error: 'Debe seleccionar al menos un área'
 		})
-    })
-    .superRefine((data, ctx) => {
-        if (data.house_type === 'departamento' && data.department_annotation === '') {
+	})
+	.superRefine((data, ctx) => {
+		if (data.house_type === 'departamento' && data.department_annotation === '') {
 			ctx.addIssue({
 				message: 'Missing annotation',
 				code: 'unrecognized_keys',
@@ -76,7 +76,7 @@ export const PersonalForm = z
 				path: ['department_annotation'],
 			});
 		}
-        if (data.house_type === 'departamento' && !data.department_number) {
+		if (data.house_type === 'departamento' && !data.department_number) {
 			ctx.addIssue({
 				message: 'Missing numeration',
 				code: 'unrecognized_keys',
@@ -85,6 +85,6 @@ export const PersonalForm = z
 			});
 		}
 		return z.NEVER;
-    });
+	});
 
 export type PersonalFormData = z.infer<typeof PersonalForm>;

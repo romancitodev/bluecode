5620ea2bf16d71826ffe1710653681a684cf6069
ChirujanoCodding <trdfabris@gmail.com>
2023-11-11 18:09:55 -0300

    feat: :construction: expanding API endpoints

--- src/app/api/areas/route.ts
+++ src/app/api/areas/route.ts
@@ -1,47 +1,27 @@
 import prisma from '@/prisma/singleton';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { AreaForm, type AreaFormData } from '@/app/schemas/area-form';
+import { ZodError } from 'zod';
 
-type Area = {
-	areaname: string;
-	incharge: string;
-	beds: number;
-	variant: 'open' | 'occupied';
+type FlattenZodError = {
+	[x in keyof AreaFormData]: string[] | undefined;
 };
+
+type Response = { message: string | { errors: FlattenZodError } };
 
 export const GET = async (request: NextRequest) => {
 	const params = request.nextUrl.searchParams;
-	const name = params.get('name');
-	console.log({ name });
 	const data = await prisma.area.findMany({
 		select: {
-			_count: {
-				select: {
-					rel_area_cama: true,
-				},
-			},
+			_count: { select: { rel_area_cama: true } },
+			rel_area_cama: { select: { cama: { select: { estado: true } } } },
 			descripcion: true,
-			rel_area_cama: {
-				select: {
-					cama: {
-						select: {
-							estado: true,
-						},
-					},
-				},
-			},
 			rel_area_usuario: {
 				select: {
 					usuario: {
 						select: {
 							rel_usuario_datos: {
-								select: {
-									datos_usuario: {
-										select: {
-											name: true,
-											surname: true,
-										},
-									},
-								},
+								select: { datos_usuario: { select: { name: true, surname: true } } },
 							},
 						},
 					},
@@ -49,6 +29,7 @@ export const GET = async (request: NextRequest) => {
 			},
 		},
 	});
+
 	const mapped = data.map(
 		({ descripcion: areaname, _count, rel_area_cama, rel_area_usuario }) => {
 			const { name, surname } =
@@ -65,4 +46,31 @@ export const GET = async (request: NextRequest) => {
 	);
 
 	return Response.json({ data: mapped });
+};
+
+export const POST = async (
+	request: NextRequest,
+): Promise<NextResponse<Response>> => {
+	const { birth, ...data } = await request.json();
+	try {
+		const area = AreaForm.parse({ ...data, birth: new Date(birth) });
+
+		await prisma.area.create({
+			data: {
+				descripcion: area.name,
+				piso: area.floor,
+			},
+		});
+
+		return NextResponse.json({ message: 'inserted' });
+	} catch (err) {
+		if (err instanceof ZodError) {
+			const { fieldErrors } = err.flatten();
+			return NextResponse.json(
+				{ message: { errors: fieldErrors as FlattenZodError } },
+				{ status: 400 },
+			);
+		}
+		return NextResponse.json({ message: err as string }, { status: 500 });
+	}
 };
--- src/app/api/personal/route.ts
+++ src/app/api/personal/route.ts
@@ -1,7 +1,23 @@
+import {
+	PersonalForm,
+	type PersonalFormData,
+} from '@/app/schemas/personal-form';
 import prisma from '@/prisma/singleton';
-import { NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async (request: Request) => {
 	const data = await prisma.datos_usuario.findMany();
 	return Response.json({ data });
+};
+
+type FlattenZodError = {
+	[x in keyof PersonalFormData]: string[] | undefined;
+};
+
+type Response = { message: string | { errors: FlattenZodError } };
+
+export const POST = async (
+	request: NextRequest,
+): Promise<NextResponse<Response>> => {
+	return NextResponse.json({ message: 'i got u' });
 };

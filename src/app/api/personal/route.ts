762fa86219e072cc7e76b3785f097a324adc5944
tamagoru <108638495+ztamagoru@users.noreply.github.com>
2023-11-14 18:15:49 -0300

    feat(back): :construction: working on 'create' button functionality of personal modal
    pushing to work on pc

--- src/app/api/personal/route.ts
+++ src/app/api/personal/route.ts
@@ -1,9 +1,14 @@
+import Personal from '@/app/(dashboard)/personal/page';
 import {
 	PersonalForm,
 	type PersonalFormData,
 } from '@/app/schemas/personal-form';
 import prisma from '@/prisma/singleton';
 import { NextRequest, NextResponse } from 'next/server';
+
+BigInt.prototype.toJSON = function () {
+	return this.toString()
+}
 
 export const GET = async (request: Request) => {
 	const data = await prisma.datos_usuario.findMany();
@@ -19,5 +24,18 @@ type Response = { message: string | { errors: FlattenZodError } };
 export const POST = async (
 	request: NextRequest,
 ): Promise<NextResponse<Response>> => {
-	return NextResponse.json({ message: 'i got u' });
+	const data = await request.json();
+	console.log(data);
+
+	try {
+		const user = PersonalForm.parse({ ...data, birth: new Date(data.birth) });
+
+		await prisma.$executeRaw`CALL crear_usuario(${user.mail}, ${user.password}, 'Enfermero', ${user.dni}, ${user.name}, ${user.surname}, ${user.cuit}, ${user.phone}, ${user.civil_status}, ${user.date_start}, ${user.birth}, 'A+', '', ${user.street}, ${user.street_number}, ${user.neighborhood}, ${user.province}, ${user.house_type}, ${user.department_annotation}, ${user.department_number}, ${user.area})`;
+
+		for (const area in user.area) {
+			await prisma.$executeRaw`CALL añadir`
+		}
+	} catch {
+
+	}
 };

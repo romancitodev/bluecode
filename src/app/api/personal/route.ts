f1b9e47cdfcab3fda2dab45671ad799f658cd6b2
ztamagoru <zdamaria119@gmail.com>
2023-11-15 19:39:38 -0300

    feat(back): :construction: trying to fetch info from db
    currently not working

--- src/app/api/personal/route.ts
+++ src/app/api/personal/route.ts
@@ -11,9 +11,56 @@ BigInt.prototype.toJSON = function() {
 	return this.toString();
 };
 
-export const GET = async (request: Request) => {
-	const data = await prisma.datos_usuario.findMany();
-	return Response.json({ data });
+export const GET = async (request: NextRequest) => {
+	const { searchParams: query } = request.nextUrl;
+
+	if (!query.get('dni') || query.get('dni') === '0') {
+		const data = await prisma.datos_usuario.findMany();
+		return Response.json({ data });
+	} else {
+		const dni = query.get('dni');
+		const data = await prisma.usuario.findFirst({
+			select: {
+				name: true,
+				surname: true,
+				fecha_nacimiento: true,
+				genero: true,
+				estado_civil: true,
+				telefono: true,
+				domicilio_usuario: {
+					select: {
+						calle: true,
+						numero: true,
+						localidad: true,
+						provincia: true,
+					},
+				},
+			},
+			where: { dni : Number(dni) },
+		});
+
+		if (!data)
+			return Response.json({ message: { errors: 'not found' } }, { status: 404 });
+		const { calle, localidad, prvincia, numero } = data.domicilio_paciente ?? {
+			calle: 'calle desconocida',
+			localidad: 'localidad desconocida',
+			provincia: 'provincia desconocida',
+			numero: 'numero desconocido',
+		};
+		
+		return Response.json({
+			message: {
+				name: data.name,
+				surname: data.surname,
+				birth: data.fecha_nacimiento,
+				gender: data.genero,
+				dni,
+				civil_status: data.estado_civil,
+				phone: data.telefono,
+				address: `${calle} ${numero}, ${localidad} | ${provincia}`,
+			},
+		});
+	}
 };
 
 type FlattenZodError = {
@@ -21,6 +68,7 @@ type FlattenZodError = {
 };
 
 type Response = { message: string | { errors: FlattenZodError } };
+
 
 export const POST = async (
 	request: NextRequest,
